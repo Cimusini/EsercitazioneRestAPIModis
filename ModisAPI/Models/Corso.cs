148b087e2e74b13725e53c794eea29e1f0cc6289
Cimusini <musimecisimone@gmail.com>
2018-12-10 16:49:31 +0100

    aggiunta classe corso

---
+++ ModisAPI/Models/Corso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ModisAPI.Models
+{
+    public class Corso
+    {
+        public int CorsoId { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataInizio { get; set; }
+        public int DataInOre { get; set; }
+        public int Livello { get; set; }
+        public int NumeroMassimoPartecipanti { get; set; }
+
+    }
+}
