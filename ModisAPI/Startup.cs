886bdf532bd55168827ecf8be75c68628ecfc471
Cimusini <musimecisimone@gmail.com>
2018-12-12 17:28:10 +0100

    documentazione swagger

--- ModisAPI/Startup.cs
+++ ModisAPI/Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using ModisAPI.WorkerServices;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ModisAPI
 {
@@ -27,7 +21,11 @@ namespace ModisAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddScoped<IWorkerServiceStudenti,WorkerServiceSQLServerDB>();
+            services.AddScoped<IWorkerServiceStudenti, WorkerServiceSQLServerDB>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Modis API", Version = "v1" });
+            });
 
         }
 
@@ -45,6 +43,12 @@ namespace ModisAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
